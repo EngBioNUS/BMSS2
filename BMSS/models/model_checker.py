dd98aa4e99490b54b321424a7bb0a6da65460214
russellnjk <42510132+russellnjk@users.noreply.github.com>
2020-08-13 21:18:32 +0800

    Add files via upload

---
+++ BMSS/models/model_checker.py
@@ -0,0 +1,166 @@
+import string
+from   pathlib import Path
+from   runpy   import run_path
+
+###############################################################################
+#Non-Standard Imports
+###############################################################################
+try:
+    from .model_coder import equations_to_code, model_to_code
+except:
+    from model_coder import equations_to_code, model_to_code
+    
+punctuation = string.punctuation.replace('_', '')
+
+def check_model_terms(model):
+    states    = model['states']
+    params    = model['parameters']
+    inputs    = model.get('inputs', [])
+    equations = model['equations']
+    
+    states_set = set(states)
+    params_set = set(params)
+    inputs_set = set(inputs)
+    if len(states_set) != len(states):
+       return False, 'Multiple occurence of certain states in states.'
+    if len(params_set) != len(params):
+        return False, 'Multiple occurence of certain parameters in parmeters.'
+    if len(inputs_set) != len(inputs):
+        return False, 'Multiple occurence of certain inputs in inputs.'
+    
+    shared = states_set.intersection(params_set, inputs_set)
+    if shared:
+        return False, 'Found ' + str(shared) + ' in both states, parameters and/or inputs.'
+    
+    terms    = list(map(get_terms, equations))
+    lhs, rhs = [set().union(*s) for s in zip(*terms)]
+    
+    defined = lhs.union(states_set, params_set, inputs_set)
+    
+    undefined = [term for term in rhs if term not in defined]
+    
+    if undefined:
+        return False, 'Undefined terms ' + str(undefined)
+    
+    unused = [term for term in inputs+params if term not in rhs]
+    
+    if unused:
+        return False, 'Unused terms ' + str(unused)
+    
+    else:
+        
+        # header = 'import numpy as np\ndef __model_checker_function__():'
+        # temp   = states+params+inputs
+        # var    = '\t' + ','.join(temp) + '= np.random.rand(' + str(len(temp)) + ')'
+        # conv   = '\t' + ','.join(temp) + '= list(map(float, [' + ','.join(temp) + ']))'
+        # call   = '__model_checker_function__()'
+        
+        # try:
+        #     eq     = equations_to_code(equations, states)
+        # except Exception as e:
+        #     return False, str(e.args[0])
+        
+        # test_string = '\n\n'.join([header, var, conv, eq, call])
+        
+        temp   = states+params+inputs
+        var    = ','.join(temp) + '= np.random.rand(' + str(len(temp)) + ')'
+        conv   = ','.join(temp) + '= list(map(float, [' + ','.join(temp) + ']))'
+        y_test = 'y = ' + ','.join(states)
+        t_test = 't = 0'
+        p_test = 'params = ' + ','.join(params+inputs)
+        call   = 'y = model_' + '_'.join(model['system_type']) + '(y, t, params)'
+        
+        try:
+            eq     = model_to_code(model, use_numba=False, mode=None)
+        except Exception as e:
+            return False, str(e.args[0])
+        
+        test_string = '\n\n'.join([eq, var, conv, y_test, t_test, p_test, call, call])
+        
+        filename    = Path(__file__).parent/ 'checker_do_not_run.py'
+        for i in range(10):
+            try:
+                
+                with open(filename, 'w') as file:
+                    file.write(test_string)
+                run_path(filename)
+                
+            except ZeroDivisionError:
+                if i < 9:
+                    pass
+                else:
+                    return False, 'Division by zero detected.'
+            except IndexError:
+                return False, 'Length of return value is not equal to length of states.'
+            except Exception as e:
+                return False, str(e.args[0])
+            else:
+                break
+            
+    return True, ''
+
+def get_terms(f):
+    '''
+    Supporting function for make_group. Extracts all terms in an expression.
+    '''
+    global punctuation
+    
+    fs = f.strip()
+    fs = f.split('#')[0]
+    
+    if not fs:
+        return set(), set()
+    
+    eq = [s.strip() for s in  fs.split('=', 1)]
+    
+    if len(eq) < 2:
+        raise Exception('Line is not an equation: ' + f)
+
+    if any(list(map(illegal_equal, eq))):
+        raise Exception('Syntax error. Illegal use of "=" symbol in line: '+ f)
+    
+    tr = str.maketrans(punctuation, ','*len(punctuation))
+    eq = [set([i for i in s.replace(' ', '').translate(tr).split(',') if i and not isnum(i)]) for s in eq]
+    
+    return eq
+
+def isnum(x):
+    try:
+        float(x)
+        return True
+    except:
+        pass
+    try:
+        float(x+'0')
+        return True
+    except:
+        return False
+
+def illegal_equal(x):
+    S = x.split('==')
+    if S[0] == '' or S[-1]=='':
+        return True
+    
+    for i in range(len(S)):
+        s = S[i].split('=')
+
+        if len(s) > 1:
+            return True
+    return False
+    
+if __name__ == '__main__':
+    __model__ = {'id'          : 'bmss01001',
+                 'system_type' : ['Inducible', 'ConstInd'],
+                 'states'      : ['mRNA', 'Pep'], 
+                 'parameters'  : ['syn_mRNA', 'deg_mRNA', 'syn_Pep', 'deg_Pep', 'Ki'],
+                 'inputs'      : ['Ind'],
+                 'equations'   : ['dmRNA = syn_mRNA*Ind/(Ind + Ki) - deg_mRNA*mRNA',
+                                  '',
+                                  'dPep  = syn_Pep*mRNA - 1/(deg_Pep)'
+                                  ],
+                 'ia'          : 'ia_result_bmss01001.csv'
+                 
+                 }
+    
+    r = check_model_terms(__model__)
+    print(r)
--- BMSS/models/model_coder.py
+++ BMSS/models/model_coder.py
@@ -1,1 +1,132 @@
+from os.path import dirname, join
 
+###############################################################################
+#Globals
+###############################################################################
+model_functions_directory = join(dirname(__file__), 'model_functions')
+
+###############################################################################
+#Functions
+###############################################################################
+def model_to_code(model_dict,  use_numba=True, local=False, mode='w'):
+    header = 'import numpy as np\n'
+    if use_numba:
+        header += 'from numba import jit\n\n' + '@jit(nopython=True)\n'
+    
+    result  = header + 'def model_' + '_'.join(model_dict['system_type']) + '(y, t, params):\n' 
+    
+    states     = states_to_code(model_dict['states'])
+    params     = params_to_code(model_dict['parameters'] + model_dict['inputs'])
+    equations  = equations_to_code(model_dict['equations'], model_dict['states'])
+    result    += '\n\n'.join([states, params, equations])
+    
+    filename = '_'.join(model_dict['system_type']) + '.py'
+    if mode == 'w' or mode == 'a':
+        export_code(result, filename, local, mode)
+    return result
+
+###############################################################################
+#Export
+###############################################################################
+def export_code(code, filename='code1.py', local=False, mode='w'):
+    global model_functions_directory
+    filename1 = filename if local else join(model_functions_directory, filename)
+    with open(filename1, mode) as file:
+        file.write(code)
+    return
+    
+
+###############################################################################
+#Supportying Functions
+############################################################################### 
+def states_to_code(states, indent=1):
+    longest = len(max(states, key=len))
+    temp    = ['\t'*indent + states[i] + ' '*(longest - len(states[i]) + 1) + '= y[' + str(i) + ']' for i in range(len(states))]
+    result  = '\n'.join(temp)
+    
+    return result
+    
+def params_to_code(params, indent=1):
+    longest = len(max(params, key=len))
+    temp    = ['\t'*indent + params[i] + ' '*(longest - len(params[i]) + 1) + '= params[' + str(i) + ']' for i in range(len(params))]
+    result  = '\n'.join(temp)
+    
+    return result
+
+def equations_to_code(equations, states, indent=1):
+    '''
+    Excepts a list of strings corresponding to equations and formats them.
+    Assumes the last block of strings contains the differentials.
+    
+    E.g. ["a=1","","ds=a**2","dx=a**3"]
+    will return the following.
+    "
+    a = 1
+    
+    ds = a**2
+    dx = a**3
+    
+    return np.array([ds, dx])
+    "
+    '''
+    result = ''
+    blocks = []
+    diff = []
+    expr = []
+    for equation in equations:
+        if equation.strip():
+            diff_, expr_ = [s.strip() for s in equation.split('=')]
+       
+            diff.append(diff_)
+            expr.append(expr_)
+        else:
+            if diff:
+                blocks.append([diff.copy(), expr.copy()])
+                diff = []
+                expr = []
+            else:
+                pass
+    if diff:
+        blocks.append([diff, expr])
+                
+    temp = []
+    for block in blocks:
+        if temp:
+            temp.append('\t'*indent)
+        temp.append(equations_to_code_helper(*block, indent=indent))
+    
+    return_value = '\t'*indent + 'return np.array([' + ', '.join(diff) +'])' 
+    
+    if len(diff) != len(states):
+        message = ' '.join(['The final block of equations contains', 
+                            str(len(diff)),
+                            'equations. However, the model has', 
+                            str(len(states)),
+                            'states.'
+                            ])
+        raise Exception(message)
+        
+    result  = '\n'.join(temp) + '\n\n' + return_value
+    return result 
+
+def equations_to_code_helper(diff, expr, indent=1):       
+    longest = len(max(diff)) 
+    temp    = ['\t'*indent + diff[i] + ' '*(longest - len(diff[i]) + 1) + '= ' + expr[i] for i in range(len(diff))]
+    result  = '\n'.join(temp)
+    return result
+    
+if __name__ == '__main__':
+    __model__ = {'system_type' : ['Inducible', 'ConstInd'],    
+                 'variables'   : ['Ind', 'mRNA', 'Pep', 'syn_mRNA', 'deg_mRNA', 'syn_Pep', 'deg_Pep', 'Ki'],
+                 'states'      : ['mRNA', 'Pep'], 
+                 'parameters'  : ['syn_mRNA', 'deg_mRNA', 'syn_Pep', 'deg_Pep', 'Ki'],
+                 'inputs'      : ['Ind'],
+                 'equations'   : ['dmRNA  = syn_mRNA*Ind/(Ind + Ki)     - deg_mRNA*mRNA',
+                                  'dPep   = syn_Pep*mRNA - deg_Pep'
+                                  ]
+                 }
+    
+#    code = model_to_code(__model__)
+#    export_code(code)
+    
+    export_models([__model__])
