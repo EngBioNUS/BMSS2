6b434af2447f5b80792b93003546849470641c09
russellnjk <42510132+russellnjk@users.noreply.github.com>
2020-08-23 16:59:26 +0800

    Add files via upload

---
+++ BMSS/models/model_functions/BMSS_Monod_Constitutive_Single_ProductInhibition.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numba import jit
+
+@jit(nopython=True)
+def model_BMSS_Monod_Constitutive_Single_ProductInhibition(y, t, params):
+	x = y[0]
+	s = y[1]
+	h = y[2]
+
+	mu_max = params[0]
+	Ks     = params[1]
+	Kh     = params[2]
+	Y      = params[3]
+	synh   = params[4]
+
+	mu = mu_max*s/(s+Ks)*Kh/(h+Kh)
+	
+	dx = x*mu
+	ds = -dx/Y
+	dh = synh -h*mu
+
+	return np.array([dx, ds, dh])
